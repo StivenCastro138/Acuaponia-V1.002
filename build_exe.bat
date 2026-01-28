5c67a39f5d83d93816d7d110740368373d46c7ee
StivenCastro138 <84600867+StivenCastro138@users.noreply.github.com>
2026-01-27 19:15:45 -0500

    Update build_exe.bat

--- build_exe.bat
+++ build_exe.bat
@@ -1,15 +1,15 @@
 @echo off
-title Compilador FishTrace V1.2 
+title Compilador FishTrace V1.2
 set "APP_NAME=FishTrace"
 set "EXE_PATH=%CD%\dist\%APP_NAME%\%APP_NAME%.exe"
 set "ICO_PATH=%CD%\logo.ico"
-
 set "DESKTOP_PATH=%USERPROFILE%\Desktop\%APP_NAME%.lnk"
 if exist "%USERPROFILE%\OneDrive\Escritorio" set "DESKTOP_PATH=%USERPROFILE%\OneDrive\Escritorio\%APP_NAME%.lnk"
 
 echo ===========================================
 echo 1. Preparando entorno y dependencias
 echo ===========================================
+
 py -3.11 -m pip install --upgrade pip
 py -3.11 -m pip uninstall pyqt6 pyqt6-plugins pyqt6-qt6 -y
 py -3.11 -m pip install pyinstaller pyqtdarktheme==2.1.0 darkdetect
@@ -22,7 +22,6 @@ if exist dist rd /s /q dist
 
 echo.
 echo 3. Generando ejecutable (.exe) con icono...
-REM 
 py -3.11 -m PyInstaller --noconfirm --onedir --windowed ^
  --name "%APP_NAME%" ^
  --icon "%ICO_PATH%" ^
@@ -32,20 +31,6 @@ py -3.11 -m PyInstaller --noconfirm --onedir --windowed ^
  --add-data "BasedeDatos;BasedeDatos" ^
  --add-data "Herramientas;Herramientas" ^
  "app.py"
-
-REM 
-if %ERRORLEVEL% NEQ 0 (
-    color 4F
-    echo.
-    echo ===========================================
-    echo ERROR CRITICO: La compilacion ha fallado.
-    echo No se ha creado el archivo .exe.
-    echo Revise los errores arriba (letras rojas).
-    echo ===========================================
-    pause
-    exit /b
-)
-color 07
 
 echo.
 echo 4. Creando acceso directo con icono en el Escritorio...
@@ -58,14 +43,8 @@ powershell -ExecutionPolicy Bypass -Command ^
  "$s.Save()"
 
 echo.
-echo 5. Iniciando aplicacion automaticamente...
-if exist "%EXE_PATH%" (
-    start "" "%EXE_PATH%"
-) else (
-    echo Error: No se encuentra el archivo ejecutable.
-    pause
-)
-
+echo 5. Iniciando aplicacion automáticamente...
+start "" "%EXE_PATH%"
 echo ===========================================
 echo PROCESO FINALIZADO EXITOSAMENTE
 echo ===========================================
